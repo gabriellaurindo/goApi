7602e600e6a3ffee6184af28416524c8b74834c7
Gabriel Laurindo <gabrielvitorlaurindo@gmail.com>
2021-09-05 18:46:35 -0300

    Teste

--- src/main.go
+++ src/main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
-func main() {
-	port := os.Getenv("PORT")
+func hello(w http.ResponseWriter, req *http.Request) {
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
 	}
+}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+func main() {
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
 }
